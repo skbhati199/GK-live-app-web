b1a9a2509000b94ae62abaf5194143a5e7b2b590
skbhati <skbhati199@gmail.com>
2018-02-21 17:32:05 +0530

    work find

--- src/app/question-create/question-create.component.ts
+++ src/app/question-create/question-create.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-question-create',
   templateUrl: './question-create.component.html',
-  styleUrls: ['./question-create.component.css']
+  styleUrls: ['./question-create.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class QuestionCreateComponent implements OnInit {
 
-  constructor() { }
+  question  = {
+    "questionTitle":"",
+    "publisher":""
+  };
+
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  saveBook() {
+    this.http.post('/question', this.question)
+      .subscribe(res => {
+          let id = res['_id'];
+          this.router.navigate(['/question-details', id]);
+        }, (err) => {
+          console.log(err);
+        }
+      );
   }
 
 }

cebbb433749ac60d2aad3acc56e5c6f580f850fd
skbhati <skbhati199@gmail.com>
2018-02-22 10:43:50 +0530

    change routes

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':'false'}));
 app.use(express.static(path.join(__dirname, 'dist')));
-app.use('/', express.static(path.join(__dirname, 'dist')));
+app.use('/questions', express.static(path.join(__dirname, 'dist')));
 app.use('/question', question);
 
 // catch 404 and forward to error handler
